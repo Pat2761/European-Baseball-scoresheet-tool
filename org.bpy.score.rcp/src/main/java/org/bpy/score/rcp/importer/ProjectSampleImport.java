35cd82e6e0c2f265318e70d5008be8c6505d7317
bpy1401 <briand.patrick@free.fr>
2025-12-03 09:10:44 +0100

    Create sample plugins

---
+++ org.bpy.score.rcp/src/main/java/org/bpy/score/rcp/importer/ProjectSampleImport.java
@@ -0,0 +1,25 @@
+package org.bpy.score.rcp.importer;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class ProjectSampleImport extends Wizard implements IImportWizard {
+
+	public ProjectSampleImport() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ org.bpy.score.samples.tests/src/org/bpy/score/samples/tests/EmptyTest.java
@@ -0,0 +1,12 @@
+package org.bpy.score.samples.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class EmptyTest {
+
+	@Test
+	public void emptyTest() {
+		
+	}
+
+}
